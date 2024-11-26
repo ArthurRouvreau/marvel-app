67b2a45a1788387b0a2434f06d744f0ee6fe530a
Arthur Rouvreau <arthur.rouvreau@etu.univ-poitiers.fr>
2024-11-26 18:23:41 +0100

    26/11

--- src/pages/CharacterDetailPage.test.jsx
+++ src/pages/CharacterDetailPage.test.jsx
@@ -1,48 +1,75 @@
-import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { useLoaderData } from 'react-router';
 import CharacterDetailPage from './CharacterDetailPage';
+import { BrowserRouter } from 'react-router-dom';
 
-// Mock the useLoaderData hook
+// fix for ResizeObserver not being defined in Jest
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+    delete window.ResizeObserver;
+    window.ResizeObserver = jest.fn().mockImplementation(() => ({
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+        disconnect: jest.fn()
+    }));
+});
+
+afterEach(() => {
+    window.ResizeObserver = ResizeObserver;
+    jest.restoreAllMocks();
+});
+
+// end fix for ResizeObserver not being defined in Jest
+
+const character = {
+    id: "1",
+    name: "Thor",
+    description: "Thor description",
+}
+
+// mock the useLoaderData hook, so that we can test the CharacterDetailPage component
 jest.mock('react-router', () => ({
-    useLoaderData: jest.fn(),
+    ...jest.requireActual('react-router'), // use actual for all non-hook parts
+    useLoaderData: () => {
+        return character;
+    },
 }));
- 
-describe('CharacterDetailPage', () => {
-    const character = {
-        name: 'Thor',
-        description: 'God of Thunder',
-        modified: '2023-10-01',
-        thumbnail: { path: 'path/to/image', extension: 'jpg' },
-        capacities: {
-            force: 5,
-            intelligence: 8,
-            durability: 6,
-            energy: 6,
-            speed: 1,
-            fighting: 3
-        }
-    };
 
-    beforeEach(() => {
-        useLoaderData.mockReturnValue(character);
-    });
+describe('CharacterDetailPage', () => {
 
     test('render CharacterDetailPage component', () => {
-        render(<CharacterDetailPage />);
-        expect(document.title).toBe('Thor | Marvel App');
+        // when
 
-        const nameElement = screen.getByText(character.name);
-        expect(nameElement).toBeInTheDocument();
+        // then
+        render(<CharacterDetailPage />, { wrapper: BrowserRouter });
 
-        const descriptionElement = screen.getByText(character.description);
-        expect(descriptionElement).toBeInTheDocument();
+        // expect the document title to be "Thor | Marvel App"
+        expect(document.title).toBe(`${character.name} | Marvel App`);
 
-        const modifiedElement = screen.getByText(character.modified);
-        expect(modifiedElement).toBeInTheDocument();
+        // expect to have a heading with the character name
+        const h2Element = screen.getByRole('heading', { level: 2, name: character.name });
+        expect(h2Element).toBeInTheDocument();
 
-        const imageElement = screen.getByAltText(character.name);
-        expect(imageElement).toBeInTheDocument();
-        expect(imageElement).toHaveAttribute('src', 'path/to/image/standard_large.jpg');
+        // expect to have a paragraph with the character description
+        const pElement = screen.getByText(character.description);
+        expect(pElement).toBeInTheDocument();
+
+        // expect to have a heading with the text "Capacities"
+        const h2CapacitiesElement = screen.getByRole('heading', { level: 2, name: 'Capacities' });
+        expect(h2CapacitiesElement).toBeInTheDocument();
+
+        // expect to have a heading with the text "Using D3"
+        const h3D3Element = screen.getByRole('heading', { level: 3, name: 'Using D3' });
+        expect(h3D3Element).toBeInTheDocument();
+
+        // expect to have a heading with the text "Using Recharts"
+        const h3RechartsElement = screen.getByRole('heading', { level: 3, name: 'Using Recharts' });
+        expect(h3RechartsElement).toBeInTheDocument();
+
+        // expect to have a div with the id "pie-container"
+        expect(document.getElementById('pie-container')).toBeInTheDocument();
+
+        // expect to a an div with class "recharts-wrapper"
+        expect(document.querySelector('.recharts-wrapper')).toBeInTheDocument();
     });
 });
